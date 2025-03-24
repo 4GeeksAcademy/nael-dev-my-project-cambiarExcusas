64296466716f0163e3fb68f031bd1a2ccce63960
Nael <naeldiaz@proton.me>
2025-03-24 18:15:45 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -5,20 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let quien=["Mi hermano ","El perro ", "Mi pajaro ","Mi abuelo "];
-let accion=[" se comió ","chupó ","rompió "," lavó "];
-let que =["los deberes ","el móvil ","la ropa "];
-let cuando= ["anoche cuando dormia.","mientras desayunaba.","jugando a la consola.","mientras se estaba duchando."];
+let who =["Mi hermano ","El perro ", "Mi pajaro ","Mi abuelo "];
+let action =[" se comió ","chupó ","rompió "," lavó "];
+let what =["los deberes ","el móvil ","la ropa "];
+let when = ["anoche cuando dormia.","mientras desayunaba.","jugando a la consola.","mientras se estaba duchando."];
 
 
-function frase(){
-  let frase=["La pagina falló debido a que: "];
+const randonNumber = (length) =>{
+   return Math.floor(Math.random() * (length-1));
+}
 
-    frase += quien[Math.floor(Math.random()*4)];
-    frase+= accion[Math.floor(Math.random()*4)];
-    frase+= que[Math.floor(Math.random()*3)];
-    frase+= cuando[Math.floor(Math.random()*4)];
-    return frase;
+const joinPhrase=()=>{
+  let phrase=["La pagina falló debido a que: "];
+
+    phrase += who[randonNumber(who.length)];
+    phrase += action[randonNumber(action.length)];
+    phrase += what[randonNumber(what.length)];
+    phrase += when[randonNumber(when.length)];
+    return phrase;
 
   }
 
@@ -26,5 +30,5 @@ function frase(){
 window.onload = function() {
   //write your code here
  let texto = document.getElementById("excuse");
- texto.innerHTML = frase();
+ texto.innerHTML = joinPhrase();
 };
