f8c67375b8448f247ac5c425b5b69b66d3173d7c
Nael <naeldiaz@proton.me>
2025-03-31 17:54:36 +0000

    Corrigiendo errores, de buenas prácticas

--- src/app.js
+++ src/app.js
@@ -5,30 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let who =["Mi hermano ","El perro ", "Mi pajaro ","Mi abuelo "];
-let action =[" se comió ","chupó ","rompió "," lavó "];
-let what =["los deberes ","el móvil ","la ropa "];
-let when = ["anoche cuando dormia.","mientras desayunaba.","jugando a la consola.","mientras se estaba duchando."];
 
 
-const randonNumber = (length) =>{
-   return Math.floor(Math.random() * (length-1));
+
+const randonNumber = (length) => {
+  return Math.floor(Math.random() * (length - 1));
 }
 
-const joinPhrase=()=>{
-  let phrase=["La pagina falló debido a que: "];
+const joinPhrase = () => {
+  let who = ["Mi hermano", "El perro", "Mi pajaro", "Mi abuelo"];
+  let action = ["se comió", "chupó", "rompió", "lavó"];
+  let what = ["los deberes", "el móvil", "la ropa"];
+  let when = ["anoche cuando dormia.", "mientras desayunaba.", "jugando a la consola.", "mientras se estaba duchando."];
+  let wordSecuence = ["La pagina falló debido a que: "];
 
-    phrase += who[randonNumber(who.length)];
-    phrase += action[randonNumber(action.length)];
-    phrase += what[randonNumber(what.length)];
-    phrase += when[randonNumber(when.length)];
-    return phrase;
+  wordSecuence += who[randonNumber(who.length)];
+  wordSecuence += " ";
+  wordSecuence += action[randonNumber(action.length)];
+  wordSecuence += " ";
+  wordSecuence += what[randonNumber(what.length)];
+  wordSecuence += " ";
+  wordSecuence += when[randonNumber(when.length)];
+  return wordSecuence;
 
-  }
+}
 
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
- let texto = document.getElementById("excuse");
- texto.innerHTML = joinPhrase();
+  let unitedPhrase = document.getElementById("excuse");
+  unitedPhrase.innerHTML = joinPhrase();
 };
